fdc494d28f9486090dc3a08f0cfd1c42957265a5
anisazahramufidah <anisazahramufidah.07@gmail.com>
2024-12-17 21:10:53 +0700

    anisa yang commit

---
+++ living-health-main/utils/index.tsx
@@ -0,0 +1,8 @@
+export const token = "ouQZd5aZAenyvg-hoV1FlnM9yIf4fc-M";
+export const baseURL = "https://tech-class.datacore.machinevision.global/items/Usr_Living_Things_Healthy";
+export const url_user = 'items/user';
+
+export const headers = {
+    Authorization: 'Bearer ${token}',
+    "Content-Type": "application/json",
+};
